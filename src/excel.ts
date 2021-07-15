675eb895bf5987375c02d4c18137cf69cc84a076
cjrosa23 <85596681+cjrosa23@users.noreply.github.com>
2021-07-15 05:34:40 -0700

    Update excel.ts

--- src/excel.ts
+++ src/excel.ts
@@ -1,1 +1,88 @@
+const NOMINAL_COLOR = ['#E79F00', '#029F73', '#0072B2', '#CB7AA7', '#D45E00', '#57B4E9', '#EFE441' /*'#000000'*/];
 
+const ExcelThemeMarkCommonStyle = {
+    color: NOMINAL_COLOR[0],
+    size: 1,
+    stroke: 'black',
+    strokeWidth: 0,
+    opacity: 1,
+    nominalColorRange: NOMINAL_COLOR,
+    quantitativeSizeRange: [2, 6]
+};
+
+export const excel = {
+    base: 'excel',
+
+    root: {
+        background: 'white',
+        titleColor: 'black',
+        subtitleColor: 'gray',
+        mousePositionColor: '#000000'
+    },
+
+    track: {
+        titleColor: 'black',
+        titleBackground: 'white',
+        outline: 'black',
+        outlineWidth: 1
+    },
+
+    legend: {
+        background: 'white',
+        backgroundOpacity: 0.7,
+        labelColor: 'black',
+        backgroundStroke: '#DBDBDB',
+        tickColor: 'black'
+    },
+
+    axis: {
+        tickColor: 'black',
+        labelColor: 'black',
+        baselineColor: 'black',
+        gridColor: '#E3E3E3',
+        gridStrokeWidth: 1
+    },
+
+    markCommon: {
+        ...ExcelThemeMarkCommonStyle
+    },
+    point: {
+        ...ExcelThemeMarkCommonStyle,
+        size: 3
+    },
+    rect: {
+        ...ExcelThemeMarkCommonStyle
+    },
+    triangle: {
+        ...ExcelThemeMarkCommonStyle
+    },
+    area: {
+        ...ExcelThemeMarkCommonStyle
+    },
+    line: {
+        ...ExcelThemeMarkCommonStyle
+    },
+    bar: {
+        ...ExcelThemeMarkCommonStyle
+    },
+    rule: {
+        ...ExcelThemeMarkCommonStyle,
+        strokeWidth: 1
+    },
+    link: {
+        ...ExcelThemeMarkCommonStyle,
+        strokeWidth: 1
+    },
+    text: {
+        ...ExcelThemeMarkCommonStyle,
+        textAnchor: 'middle',
+        textFontWeight: 'normal'
+    },
+    brush: {
+        ...ExcelThemeMarkCommonStyle,
+        color: 'gray',
+        opacity: 0.3,
+        stroke: 'black',
+        strokeWidth: 1
+    }
+}
