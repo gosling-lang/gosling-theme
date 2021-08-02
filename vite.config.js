fd109ed1bb46773eb85510848b5270fe3d9e31eb
Trevor Manz <trevor.j.manz@gmail.com>
2021-08-02 18:34:25 -0400

    Unify build and dev with vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: "demo",
+  resolve: {
+    alias: {
+      "gosling-theme": resolve(__dirname, "./src/index.ts"),
+    },
+  },
+  build: {
+    target: "esnext",
+    // minify: true,
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      format: ["esm", "umd"],
+      name: "goslingTheme",
+    },
+  },
+});
